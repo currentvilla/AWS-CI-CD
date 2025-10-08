d8d17a47c3fe3e0925500f64d7b622e35e20e92d
currentvilla <atamaort@icloud.com>
2025-10-08 18:21:40 +0900

    workflow and playbook

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "public_ec2a" {
-  count         = 2
+  count         = 1
   ami           = "ami-025bbcfb04b076789"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_a2.id
@@ -7,11 +7,9 @@ resource "aws_instance" "public_ec2a" {
 
   vpc_security_group_ids = [aws_security_group.public_sg2.id]
 
-  associate_public_ip_address = false
+  associate_public_ip_address = true
 
   tags = {
     Name = "Public-EC2a"
   }
 }
-
-# PR trigger: noop (no functional change)
